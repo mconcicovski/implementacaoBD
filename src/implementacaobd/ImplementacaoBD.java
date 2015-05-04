e37344e4bba3917717f51b4d3afec3eb09ae3cb4
mconcicovski <mateus987@msn.com>
2015-05-04 19:55:28 -0300

    versao2
    pega do consulta.txt

--- src/implementacaobd/ImplementacaoBD.java
+++ src/implementacaobd/ImplementacaoBD.java
@@ -35,9 +35,18 @@ public class ImplementacaoBD {
         // expressao regular: SELECT + qualquer quantidade de d, + d + qualquer quantidade de \n ou espaco + FROM + qlqr quaatidade de espaco ... e por ai vai
         // alguns parentenses definem os grupos, nao remova
         Pattern pattern = Pattern.compile("SELECT ((\\d,)*\\d)(\\n|\\s)+FROM(\\s)+(\\w+.txt)\\s+JOIN\\s+(\\w+.txt)\\s+ON\\s+(\\d+)=(\\d+)");
-        String teste = "SELECT 1,2,3,4,5 \n FROM arq1.txt JOIN arq2.txt ON 4=5";
+
+        byte[] wikiArray;
+        String teste="";
+        try {
+            wikiArray = Files.readAllBytes(Paths.get("consulta.txt"));
+            teste = new String(wikiArray, "Cp1252");
+        } catch (IOException ex) {
+            Logger.getLogger(ImplementacaoBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //  String teste = "SELECT 1,2,3,4,5 \n FROM arq1.txt JOIN arq2.txt ON 4=5"; se quiser testar, comenta ali em cima
         Matcher matcher = pattern.matcher(teste);
-        String valoresCSV = "";
+
         if (matcher.matches()) {
             System.out.println("\n grupo 1: " + matcher.group(1) + "\n grupo 2: " + matcher.group(5) + "\n grupo 3: " + matcher.group(6) + " \n grupo 4: " + matcher.group(7) + " \n grupo 5: " + matcher.group(8));
         } else {
@@ -48,13 +57,12 @@ public class ImplementacaoBD {
         // matcher.group(6) = nome do segundo arquivo
         // matcher.group(7) = nome do primeiro argumento do ON
         // matcher.group(8) = nome do segundo argumento do ON
-        valoresCSV = matcher.group(1);
+        String valoresCSV = matcher.group(1);
         String[] valoresSelectstring = valoresCSV.split(",");
         List<Integer> valoresSelectint = new ArrayList<Integer>();
         List<String> arquivo1Linhas = new ArrayList<String>();
         List<String> arquivo2Linhas = new ArrayList<String>();
         try {
-
             arquivo1Linhas = Files.readAllLines(Paths.get(matcher.group(5)), Charset.forName("Cp1252")); // abre arquivo 1, divide em linhas e coloca na lista
             arquivo2Linhas = Files.readAllLines(Paths.get(matcher.group(6)), Charset.forName("Cp1252")); // abre  arquivo 2, divide em linhas e coloca na lista
         } catch (IOException ex) {
@@ -62,28 +70,29 @@ public class ImplementacaoBD {
             return;
         }
 
-        List<List<String>> arquivo1 = new ArrayList<List<String>>(); 
+        List<List<String>> arquivo1 = new ArrayList<List<String>>();
         List<List<String>> arquivo2 = new ArrayList<List<String>>();
-        int index = 0;
+
         for (String s : arquivo1Linhas) {
             arquivo1.add(Arrays.asList(s.split(","))); // pega cada linha do arquvo 1 e coloca em uma lista de string, cada string equivale a 1 coluna
         }
         for (String s : arquivo2Linhas) {
             arquivo2.add(Arrays.asList(s.split(","))); // pega cada linha do arquvo 2 e coloca em uma lista de string, cada string equivale a 1 coluna
         }
-
         Integer control = 0;
         for (String s : valoresSelectstring) {
             valoresSelectint.add(Integer.valueOf(s));
             if (control % 2 == 0) {
                 if (Integer.valueOf(s) > arquivo1.get(0).size()) {
                     System.out.println("ERRO: Coluna select maior que o possivel");
+                    return;
                 }
             }
             if (control % 2 == 1) {
-               if (Integer.valueOf(s)> arquivo2.get(0).size()) {
+                if (Integer.valueOf(s) > arquivo2.get(0).size()) {
                     System.out.println("ERRO: Coluna select maior que o possivel");
-               }
+                    return;
+                }
             }
             control++;
         }
@@ -91,9 +100,11 @@ public class ImplementacaoBD {
         Integer on2 = Integer.valueOf(matcher.group(8));
         if (on1 > arquivo1.get(0).size()) {
             System.out.println("ERRO: ON com coluna maior que o possivel no arquvo 1");
+            return;
         }
         if (on2 > arquivo2.get(0).size()) {
             System.out.println("ERRO: ON com coluna maior que o possivel no arquvo 2");
+            return;
         }
 
     }
